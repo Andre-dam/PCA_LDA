846a3888d9f6112098f0a703a0861f6b3c3aa70e
aldsj <aldsj@cin.ufpe.br>
2018-05-27 13:49:28 -0300

    tune

--- PCA.py
+++ PCA.py
@@ -4,20 +4,22 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 def pca(X,K):
-    #mean removal of each feature
     X=X.astype('float64')
+    #------------------mean removal of each feature-----------------------
     X_norm = preprocessing.scale(X, with_std = False)
-    #covariance matrix a transposicao pois linha= variavel coluna = observacao
-    X_cov = 1.0/len(X_norm) * np.matmul(X_norm.T , X_norm)
 
+    #----------------------covariance matrix------------------------------
+    X_cov = 1.0/len(X_norm) * np.matmul(X_norm.T , X_norm)
+    
+    #------------eigenvalues and eigenvectors computation-----------------
     eigenValues, eigenVectors = np.linalg.eig(X_cov)
 
-    #sort eigenvalues and eigenvectors descending
+    #-----------sort eigenvalues and eigenvectors descending--------------
     idx = eigenValues.argsort()[::-1]   
     eigenValues = eigenValues[idx]
     eigenVectors = eigenVectors[:,idx]
-    #print eigenValues
+
+    #------------------------dimension change-----------------------------
     Z = np.matmul(X_norm,eigenVectors[:,range(K)])
 
-    
     return Z
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PCA import pca
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 #from sklearn.decomposition import PCA
 
 	#plt.scatter(Z[:,0],Z[:,1])
@@ -18,19 +19,13 @@ def main():
 	n = dataset_instances.shape[0]
 
 	for K in range(1,dataset_instances.shape[1]+1):
-		#-------------------------------Aplicacao do PCA------------------------------
+
+		#-------------------------------PCA application-------------------------------
 		Z = pca(dataset_instances,K)
-		#---------------Particionando o dataset em 30% teste 70% treino------------------------------
-		trainset_instances = Z[range(int(n*0.9)),:]
-		testset_instances = Z[range(int(n*0.9),n),:]
-		trainset_class = dataset_class[range(int(n*0.9))]
-		testset_class = dataset_class[range(int(n*0.9),n)]
-		
-		#-------------------------------------------Verificacao de acuracia com 3-nn------------------------------------------------
-		#treinamento
-		knn = KNeighborsClassifier(n_neighbors=9)  
-		knn.fit(trainset_instances , trainset_class)
-		#score
-		score = knn.score(testset_instances,testset_class) * 100
-		print "Para "+repr(K)+" Autovetor(es)"+" Acuracia media 3-nn: "+repr(score) + '%'
+		#-------------------------------cross validation------------------------------
+		knn = KNeighborsClassifier(n_neighbors=3) 
+		score = cross_val_score(knn, Z, dataset_class, cv=10)
+
+		print "For "+repr(K)+" Eigenvectors: "+" Accuracy using 3-nn = "+repr(score.mean()*100) + '%'
+
 main()
